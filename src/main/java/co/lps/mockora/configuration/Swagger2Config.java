6ff19f5da8f4519041e031642d334268a5ff64f9
Joseph Gonzales <josephgonzales01@gmail.com>
2019-07-06 23:32:24 +1200

    use swagger

---
+++ src/main/java/co/lps/mockora/configuration/Swagger2Config.java
@@ -0,0 +1,27 @@
+package co.lps.mockora.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * co.lps.mockora.configuration
+ *
+ * @author : josephg
+ * @since : 6/07/2019
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
